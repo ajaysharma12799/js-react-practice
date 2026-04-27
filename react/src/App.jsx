fcfd2dce18f3b14f4d9de29bcba91e640bfb858b
Ajay Sharma <ajaysharma12799@gmail.com>
2026-04-27 18:25:14 +0530

    added react app for solving fronted specific problems

---
+++ react/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Counter from './Counter'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Counter />
+    </Fragment>
+  )
+}
+
+export default App
